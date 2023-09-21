ca862ba7bbcdde1c810332a74127e842daf93eba
ahadjawaid <94938815+ahadjawaid@users.noreply.github.com>
2023-09-21 11:32:29 -0500

    feat: added pricing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 // Outlet
 import BaseOutlet from "@/outlets/BaseOutlet";
+import Sections from "@/outlets/Sections";
 
 // Composities
 import Hero from "@/composities/Hero"
@@ -10,15 +11,14 @@ import Companies from "@/composities/Companies";
 import Testimonials from "@/composities/Testimonials";
 
 // Constants
-import { faq, spacingStyle } from "@/config";
+import { faq, spacingStyle, backgroundColor } from "@/config";
 import CallToAction from "@/composities/CallToAction";
 
-const backgroundColor = "bg-slate-100 py-12";
-
 const sections = [
+  <Hero key={0} />,
   <Companies key={1} />,
   // <Product key={2} />,
-  // <Pricing key={3} />, 
+  <Pricing key={3} />, 
   <Testimonials key={4} />, 
   <CallToAction key={5} />, 
   <Questions key={6} questions={faq} />
@@ -27,16 +27,7 @@ const sections = [
 export default function Home() {
   return (
     <BaseOutlet>
-      <main className={`${spacingStyle}`}>
-        <Hero />
-      </main>
-      {
-        sections.map((section, index) => (
-          <section key={index} className={`${index % 2 == 0 && backgroundColor} ${spacingStyle}`}>
-            {section}
-          </section>
-        ))
-      }
+      <Sections sections={sections} backgroundColor={backgroundColor} spacingStyle={spacingStyle} />
     </BaseOutlet>
   )
 }
