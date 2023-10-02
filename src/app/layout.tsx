3804a19bfa3ab1cf2454d740e7a5cdbec6141f94
ahadjawaid <94938815+ahadjawaid@users.noreply.github.com>
2023-10-02 07:55:13 -0500

    feat: added hotjar analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,18 @@ export default function RootLayout({
             gtag('config', 'G-ZZPHMT90RC');
           `}
         </Script>
+        <Script id="hotjar">
+          {`
+            (function(h,o,t,j,a,r){
+              h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
+                    h._hjSettings={hjid:3677395,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
       </head>
       <body className="h-screen">{children}</body>
     </html>
